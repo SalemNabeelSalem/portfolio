4f44700019c2b2d38ab3bc86690f50def6c074c4
Salem Nabeel Salem <isalemnabeelsalem@gmail.com>
2022-06-11 21:24:53 +0300

    Use Animate On Scroll Library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+
+import "aos/dist/aos.css";
 
 import Card from "./components/Card";
 import About from "./components/About";
@@ -7,17 +10,27 @@ import Footer from "./components/Footer";
 import ContactMe from "./components/Contact";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      once: true,
+    });
+  });
+
   return (
     <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100">
-      <Card />
+      <div data-aos="fade-down" data-aos-duration="800">
+        <Card />
+      </div>
 
-      <About />
+      <div data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
+        <About />
 
-      <Skills />
+        <Skills />
 
-      <ContactMe />
+        <ContactMe />
 
-      <Footer />
+        <Footer />
+      </div>
     </div>
   );
 }
